130e4c5e2e129cae45bd171cea5ca90ec8d05007
Mateush24 <119892085+Mateush24@users.noreply.github.com>
2022-12-19 14:28:14 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package bsu.rfe.java.group8.lab2.DubatovkaM.var3a;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+// Конструируем экземпляр фрейма
+        JFrame frame = new JFrame();
+// Задаѐм реакцию на нажатие кнопки закрытия фрейма frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Показываем фрейм на экране
+        frame.setVisible(true);
+        frame.setSize(1200, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    }
+}
